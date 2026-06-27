9a32fe73102e9852e85ddadda17d7a3f6f9e5973
Yuvraj Sabale <yuvrajsabale999@gmail.com>
2026-06-27 14:10:17 +0530

    post deployment issue fix #1

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,24 @@
 import app from "./app.js";
 import env from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import fs from "fs";
+import path from "path";
+
 
 
 const PORT = env.PORT;
 
+console.log("CWD:", process.cwd());
+
+const uploadDir = path.join(process.cwd(), "uploads");
+
+console.log("Upload dir:", uploadDir);
+console.log("Exists:", fs.existsSync(uploadDir));
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+    console.log("Created uploads directory");
+}
 const startServer = async () => {
 
     await connectDB();
